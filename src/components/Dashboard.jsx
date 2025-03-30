3c574a251096bf79b3234e6db59e4e4822a6209a
Ariel <arie.michalik@gmail.com>
2025-03-30 22:31:35 +0200

    add prettier

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -2,7 +2,7 @@ export default function Dashboard() {
   return(
     <article className='py-4 px-8'>
       <h1 className='text-3xl font-semibold'>Zamówienia</h1>
-      <div className='flex gap-2 '>
+      <div className='flex gap-2'>
         <p>Do wyceny</p>
         <p>Wycenione</p>
         <p>Do realizacji</p>
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,77 +1,82 @@
-import { GiWoodPile } from "react-icons/gi";
-import { IconContext } from "react-icons";
-import { CiSettings, CiUser } from "react-icons/ci";
-import { MdOutlineNotificationsNone } from "react-icons/md";
-import { useState } from "react";
+import { GiWoodPile } from 'react-icons/gi';
+import { IconContext } from 'react-icons';
+import { CiSettings, CiUser } from 'react-icons/ci';
+import { MdOutlineNotificationsNone } from 'react-icons/md';
+import { useState } from 'react';
 import {
   IoAlbumsOutline,
   IoBarChartOutline,
   IoChatbubbleEllipsesOutline,
   IoHomeOutline,
-  IoSearch
-} from "react-icons/io5";
-import { LuUsers } from "react-icons/lu";
+  IoSearch,
+} from 'react-icons/io5';
+import { LuUsers } from 'react-icons/lu';
 
-export default function SideBar(){
-  return(
+export default function SideBar() {
+  return (
     <>
-      <aside className="bg-white flex flex-col items-center gap-3 py-8 px-6 ">
-        <h2 className="text-center flex justify-center items-center gap-1 text-xl"> <span><GiWoodPile /></span> DrewnoPol</h2>
-        <div className="flex justify-around items-center gap-4 my-2">
-          <div className='w-10 h-10 bg-stone-100 rounded-full flex items-center justify-center'>
-            <IconContext.Provider value={{size: "1.2em"}}>
-              <CiUser/>
+      <aside className="flex flex-col items-center gap-3 bg-white px-6 py-8">
+        <h2 className="flex items-center justify-center gap-1 text-center text-xl">
+          {' '}
+          <span>
+            <GiWoodPile />
+          </span>{' '}
+          DrewnoPol
+        </h2>
+        <div className="my-2 flex items-center justify-around gap-4">
+          <div className="flex h-10 w-10 items-center justify-center rounded-full bg-stone-100">
+            <IconContext.Provider value={{ size: '1.2em' }}>
+              <CiUser />
             </IconContext.Provider>
           </div>
-          <IconContext.Provider value={{size: "1.5em"}}>
+          <IconContext.Provider value={{ size: '1.5em' }}>
             <CiSettings />
           </IconContext.Provider>
-          <IconContext.Provider value={{size: "1.5em"}}>
+          <IconContext.Provider value={{ size: '1.5em' }}>
             <MdOutlineNotificationsNone />
           </IconContext.Provider>
-
         </div>
-        <Menu/>
+        <Menu />
       </aside>
     </>
-  )
+  );
 }
 
 const Menu = () => {
   const [isActive, setIsActive] = useState(false);
 
-  return(
-    <div className='flex flex-col justify-items-start w-full divide-y-2 divide-gray-200'>
+  return (
+    <div className="flex w-full flex-col justify-items-start divide-y-2 divide-gray-200">
       <MenuElement isActive={true} text="Szukaj">
         <IoSearch />
       </MenuElement>
-      <MenuElement isActive={isActive} text="Strona Główna" onClick = {() => setIsActive(!isActive) }>
+      <MenuElement isActive={isActive} text="Strona Główna" onClick={() => setIsActive(!isActive)}>
         <IoHomeOutline />
       </MenuElement>
-      <MenuElement isActive={isActive} text="Zamówienia" onClick = {() => setIsActive(!isActive) }>
+      <MenuElement isActive={isActive} text="Zamówienia" onClick={() => setIsActive(!isActive)}>
         <IoAlbumsOutline />
       </MenuElement>
-      <MenuElement isActive={isActive} text="Czaty" onClick = {() => setIsActive(!isActive) }>
+      <MenuElement isActive={isActive} text="Czaty" onClick={() => setIsActive(!isActive)}>
         <IoChatbubbleEllipsesOutline />
       </MenuElement>
-      <MenuElement isActive={isActive} text="Użytkownicy" onClick = {() => setIsActive(!isActive) }>
+      <MenuElement isActive={isActive} text="Użytkownicy" onClick={() => setIsActive(!isActive)}>
         <LuUsers />
       </MenuElement>
-      <MenuElement isActive={isActive} text="Podsumowania" onClick = {() => setIsActive(!isActive) }>
+      <MenuElement isActive={isActive} text="Podsumowania" onClick={() => setIsActive(!isActive)}>
         <IoBarChartOutline />
       </MenuElement>
     </div>
-  )
-}
+  );
+};
 
-
-const MenuElement = ({children, isActive, text}) => {
+const MenuElement = ({ children, isActive, text }) => {
   const bgColor = isActive ? 'bg-stone-100' : '';
-  return(
-    <div className={`flex w-full gap-4 items-center justify-items-start cursor-pointer py-3 px-4 ${bgColor}`}>
+  return (
+    <div
+      className={`flex w-full cursor-pointer items-center justify-items-start gap-4 px-4 py-3 ${bgColor}`}
+    >
       {children}
       <p>{text}</p>
     </div>
-  )
-}
-
+  );
+};
