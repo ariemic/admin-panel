2f062047bd6d8c5e806f829ae226160e35a5b79c
Ariel <arie.michalik@gmail.com>
2025-04-05 22:18:54 +0200

    update eslint.config

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import js from '@eslint/js';
 import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
+import pluginQuery from '@tanstack/eslint-plugin-query';
 
 export default [
   { ignores: ['dist'] },
@@ -23,6 +24,7 @@ export default [
     rules: {
       ...js.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
+      ...pluginQuery.configs['flat/recommended'],
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
     },
